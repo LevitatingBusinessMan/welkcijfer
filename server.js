b7e45f94b4a3a4d51661c1b50f2eccd2173c4db2
Rein F <public@reinfernhout.xyz>
2019-06-18 19:09:24 +0200

    force ssl

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-express = require('express'),
+express = require("express"),
 path = require("path"),
 bodyParser = require("body-parser"),
 config = require(path.join(__dirname,"config")),
@@ -8,11 +8,18 @@ port = 3000;
 
 app.use(bodyParser.json());
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, "/front/views"))
+if (config.force_ssl)
+    app.use((req, res, next) => {
+        if (req.headers["x-forwarded-proto"] === "http")
+            return res.redirect("https://" + req.get("host") + req.url);
+        next();
+    })
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "/front/views"))
+
+app.get("/", (req, res) => {
+    res.render("index")
 })
 
 app.post("/login", require(path.join(__dirname, "/routes/login")));
